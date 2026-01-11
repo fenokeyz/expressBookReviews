89654856d2a61b2cca72137160bad80909cbe4e8
fenokeyz <navaneethsuresh21@gmail.com>
2026-01-11 02:08:02 -0500

    done till task5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,65 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if(books){
+        return res.status(200).json(JSON.stringify(books,null,4));
+    }
+    else{
+        return res.status(404).json({message: "Books List does not Exist"})
+    }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn]);
+} else {
+    return res.status(404).json({ message: "Book not found" });
+}
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+
+    const result = Object.values(books).filter(
+        (book) => book.author === author
+    );
+
+    if (result.length > 0) {
+        return res.status(200).json(result);
+    } else {
+        return res.status(404).json({ message: "No books found for this author" });
+    }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+
+    const result = Object.values(books).filter(
+        (book) => book.title === title
+    );
+
+    if (result.length > 0) {
+        return res.status(200).json(result);
+    } else {
+        return res.status(404).json({ message: "No books found for this title" });
+    }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
 
+    if (books[isbn]) {
+        return res.status(200).json(books[isbn].reviews);
+    } else {
+        return res.status(404).json({
+            message: "Book not found"
+        });
+    }
+});
 module.exports.general = public_users;
