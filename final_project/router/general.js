9f22a122e1af874fc9b571b31ed90b2df0d36a95
fenokeyz <navaneethsuresh21@gmail.com>
2026-01-13 10:51:30 -0500

    5th update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,8 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require("axios");
+
 
 
 public_users.post("/register", (req, res) => {
@@ -32,7 +34,7 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
     if(books){
-        return res.status(200).json(JSON.stringify(books,null,4));
+        return res.status(200).send(JSON.stringify(books,null,4));
     }
     else{
         return res.status(404).json({message: "Books List does not Exist"})
@@ -93,25 +95,53 @@ public_users.get('/review/:isbn', function (req, res) {
     }
 });
 
-// Delete a book review
-regd_users.delete("/auth/review/:isbn", (req, res) => {
-    const isbn = req.params.isbn;
-    const username = req.session.authorization.username;
+// Task 10: Get all books using async/await and Axios
+public_users.get("/async/books", async (req, res) => {
+    try {
+        const response = await axios.get("http://localhost:5000/");
+        return res.status(200).send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching books" });
+    }
+});
 
-    if (!books[isbn]) {
+// Task 11: Get book details by ISBN using async/await and Axios
+public_users.get("/async/books/isbn/:isbn", async (req, res) => {
+    const isbn = req.params.isbn;
+
+    try {
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
         return res.status(404).json({ message: "Book not found" });
     }
-
-    if (!books[isbn].reviews[username]) {
-        return res.status(404).json({ message: "Review not found for this user" });
-    }
-
-    delete books[isbn].reviews[username];
-
-    return res.status(200).json({
-        message: "Review deleted successfully"
-    });
 });
+
+// Task 12: Get books by Author using async/await and Axios
+public_users.get("/async/books/author/:author", async (req, res) => {
+    const author = req.params.author;
+
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(404).json({ message: "No books found for this author" });
+    }
+});
+
+// Task 13: Get books by Title using async/await and Axios
+public_users.get("/async/books/title/:title", async (req, res) => {
+    const title = req.params.title;
+
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(404).json({ message: "No books found with this title" });
+    }
+});
+
+
 
 
 module.exports.general = public_users;
